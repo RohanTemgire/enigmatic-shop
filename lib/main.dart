7d95f905f19b187dc97b95cc95e2cc6f9a154237
RohanTemgire <temgirerrohan@gmail.com>
2022-02-07 18:56:17 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Enigmatic Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Enigmatic Shop'),
+      ),
+      body: const Center(
+        child: Text('Enigmatic Shop'),
+      ),
+    );
+  }
+}
